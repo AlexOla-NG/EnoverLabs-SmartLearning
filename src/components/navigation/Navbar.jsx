6cdb07bdbd8e88f4b350e14a8128325c78734b2d
Alex Oweka <alexanderolalekanoweka@gmail.com>
2023-02-28 18:06:09 +0100

    🧹chore: fixed spacing in navbar

--- src/components/navigation/Navbar.jsx
+++ src/components/navigation/Navbar.jsx
@@ -50,164 +50,158 @@ const Navbar = () => {
 			position="static"
 			sx={{ backgroundColor: "#fff", boxShadow: "unset" }}
 		>
-			<Container maxWidth="125rem">
-				<Toolbar disableGutters>
-					<nav>
-						<div className="container">
-							<NavLink to="/" className="navbar-title">
-								<span>Smart</span>
-								<span>Learning</span>
-							</NavLink>
+			{/* <Container maxWidth="125rem"> */}
+			<Toolbar disableGutters>
+				<nav className="nav">
+					<div className="container">
+						<NavLink to="/" className="navbar-title">
+							<span>Smart</span>
+							<span>Learning</span>
+						</NavLink>
 
-							{/* NOTE: navlist for small screen, hide navbar on signup & signin pages */}
-							{location.pathname === "/signup" ||
-							location.pathname === "/signin" ? null : (
-								<Box
+						{/* NOTE: navlist for small screen, hide navbar on signup & signin pages */}
+						{location.pathname === "/signup" ||
+						location.pathname === "/signin" ? null : (
+							<Box
+								sx={{
+									display: { xs: "flex", md: "none" },
+								}}
+							>
+								<IconButton
+									size="1.125rem"
+									aria-label="user menu"
+									aria-controls="menu-appbar"
+									aria-haspopup="true"
+									onClick={handleOpenNavMenu}
+									color="#000000"
+								>
+									<MenuIcon />
+								</IconButton>
+								<Menu
+									id="menu-appbar"
+									anchorEl={anchorElNav}
+									anchorOrigin={{
+										vertical: "bottom",
+										horizontal: "left",
+									}}
+									keepMounted
+									transformOrigin={{
+										vertical: "top",
+										horizontal: "left",
+									}}
+									open={Boolean(anchorElNav)}
+									onClose={handleCloseNavMenu}
 									sx={{
-										display: { xs: "flex", md: "none" },
+										display: {
+											xs: "block",
+											md: "none",
+										},
 									}}
 								>
-									<IconButton
-										size="1.125rem"
-										aria-label="user menu"
-										aria-controls="menu-appbar"
-										aria-haspopup="true"
-										onClick={handleOpenNavMenu}
-										color="#000000"
-									>
-										<MenuIcon />
-									</IconButton>
-									<Menu
-										id="menu-appbar"
-										anchorEl={anchorElNav}
-										anchorOrigin={{
-											vertical: "bottom",
-											horizontal: "left",
-										}}
-										keepMounted
-										transformOrigin={{
-											vertical: "top",
-											horizontal: "left",
-										}}
-										open={Boolean(anchorElNav)}
-										onClose={handleCloseNavMenu}
-										sx={{
-											display: {
-												xs: "block",
-												md: "none",
-											},
-										}}
-									>
-										{navlinks.map((navlink, index) => (
-											<MenuItem
-												key={index}
-												onClick={handleCloseNavMenu}
-												sx={{
-													textTransform: "capitalize",
-												}}
+									{navlinks.map((navlink, index) => (
+										<MenuItem
+											key={index}
+											onClick={handleCloseNavMenu}
+											sx={{
+												textTransform: "capitalize",
+											}}
+										>
+											<NavLink
+												to={navlink.link}
+												className={({ isActive }) =>
+													isActive
+														? "navbar-items-active"
+														: ""
+												}
 											>
+												{navlink.text}
+											</NavLink>
+										</MenuItem>
+									))}
+
+									{/* NOTE: only show signup & signin buttons on home page */}
+									{location.pathname === "/" && (
+										<MenuItem>
+											<div className="login-wrapper">
 												<NavLink
+													to="signin"
+													onClick={handleCloseNavMenu}
+												>
+													<WhiteBgBtn text="sign in" />
+												</NavLink>
+												<NavLink
+													to="signup"
+													onClick={handleCloseNavMenu}
+												>
+													<ColorBgBtn
+														text="sign up"
+														padding="0.5rem 1.5rem"
+													/>
+												</NavLink>
+											</div>
+										</MenuItem>
+									)}
+								</Menu>
+							</Box>
+						)}
+
+						{/* NOTE: navlist for large screen */}
+						<Box
+							sx={{
+								flexGrow: 1,
+								display: { xs: "none", md: "flex" },
+								justifyContent: "center",
+							}}
+						>
+							<div className="navbar-items">
+								{/* NOTE: hide navbar on signup & signin pages */}
+								{location.pathname === "/signup" ||
+								location.pathname === "/signin" ? null : (
+									<div className="navlinks-wrapper">
+										{navlinks.map((navlink, index) => {
+											return (
+												<NavLink
+													key={index}
 													to={navlink.link}
 													className={({ isActive }) =>
 														isActive
 															? "navbar-items-active"
 															: ""
 													}
+													onClick={handleCloseNavMenu}
 												>
 													{navlink.text}
 												</NavLink>
-											</MenuItem>
-										))}
-
-										{/* NOTE: only show signup & signin buttons on home page */}
-										{location.pathname === "/" && (
-											<MenuItem>
-												<div className="login-wrapper">
-													<NavLink
-														to="signin"
-														onClick={
-															handleCloseNavMenu
-														}
-													>
-														<WhiteBgBtn text="sign in" />
-													</NavLink>
-													<NavLink
-														to="signup"
-														onClick={
-															handleCloseNavMenu
-														}
-													>
-														<ColorBgBtn
-															text="sign up"
-															padding="0.5rem 1.5rem"
-														/>
-													</NavLink>
-												</div>
-											</MenuItem>
-										)}
-									</Menu>
-								</Box>
-							)}
-
-							{/* NOTE: navlist for large screen */}
-							<Box
-								sx={{
-									flexGrow: 1,
-									display: { xs: "none", md: "flex" },
-								}}
-							>
-								<div className="navbar-items">
-									{/* NOTE: hide navbar on signup & signin pages */}
-									{location.pathname === "/signup" ||
-									location.pathname === "/signin"
-										? null
-										: navlinks.map((navlink, index) => {
-												return (
-													<NavLink
-														key={index}
-														to={navlink.link}
-														className={({
-															isActive,
-														}) =>
-															isActive
-																? "navbar-items-active"
-																: ""
-														}
-														onClick={
-															handleCloseNavMenu
-														}
-													>
-														{navlink.text}
-													</NavLink>
-												);
-										  })}
-
-									{/* NOTE: only show signup & signin buttons on home page */}
-									{location.pathname === "/" && (
-										<div className="login-wrapper">
-											<NavLink
-												to="signin"
-												onClick={handleCloseNavMenu}
-											>
-												<WhiteBgBtn text="sign in" />
-											</NavLink>
-											<NavLink
-												to="signup"
-												onClick={handleCloseNavMenu}
-											>
-												<ColorBgBtn
-													text="sign up"
-													padding="0.5rem 1.5rem"
-												/>
-											</NavLink>
-										</div>
-									)}
-								</div>
-							</Box>
-						</div>
-					</nav>
-				</Toolbar>
-			</Container>
+											);
+										})}
+									</div>
+								)}
+								{/* NOTE: only show signup & signin buttons on home page */}
+								{location.pathname === "/" && (
+									<div className="login-wrapper">
+										<NavLink
+											to="signin"
+											onClick={handleCloseNavMenu}
+										>
+											<WhiteBgBtn text="sign in" />
+										</NavLink>
+										<NavLink
+											to="signup"
+											onClick={handleCloseNavMenu}
+										>
+											<ColorBgBtn
+												text="sign up"
+												padding="0.5rem 1.5rem"
+											/>
+										</NavLink>
+									</div>
+								)}
+							</div>
+						</Box>
+					</div>
+				</nav>
+			</Toolbar>
+			{/* </Container> */}
 		</AppBar>
 	);
 };

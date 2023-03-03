742daa346df9ce1bd63bb469691ebc99cb24e95a
Alex Oweka <alexanderolalekanoweka@gmail.com>
2023-03-03 14:30:43 +0100

    🔥feat: finished mobile screen styling

--- src/components/study plan/StudyProgress.jsx
+++ src/components/study plan/StudyProgress.jsx
@@ -1,9 +1,25 @@
 import React from "react";
+import { ReactComponent as ClockIcon } from "../../assets/svg/clock-icon.svg";
+import { ReactComponent as ChartIcon } from "../../assets/svg/chart-icon.svg";
 
-// NOTE: all links in studyProgress will redirect to the 404 page
+// NOTE: all links in studyProgress will redirect to a coming soon modal (use mui)
 
 const StudyProgress = () => {
-	return <div>StudyProgress</div>;
+	return (
+		<section className="study-progress">
+			<div className="text-details">
+				<div className="header-wrapper">
+					<h1>study progress</h1>
+					<ClockIcon />
+				</div>
+				<p>Track your progress</p>
+			</div>
+
+			<div className="chart-wrapper">
+				<ChartIcon />
+			</div>
+		</section>
+	);
 };
 
 export default StudyProgress;
--- src/views/StudyPlan.jsx
+++ src/views/StudyPlan.jsx
@@ -3,6 +3,9 @@ import Categories from "../components/study plan/Categories";
 import Schedule from "../components/study plan/Schedule";
 import StudyProgress from "../components/study plan/StudyProgress";
 
+// TODO: stopped here
+// fix responsiveness. adjust styling for 765px, 1024px,... screens
+
 const StudyPlan = () => {
 	return (
 		<main className="study-plan">

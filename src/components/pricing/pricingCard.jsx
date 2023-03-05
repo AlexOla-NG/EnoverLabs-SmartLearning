69a4bbf05ab0d8b50465a5d44617f6dc932b1ef9
Henry Okeke <iamhenryokeke@gmail.com>
2023-03-05 07:49:10 +0100

    added component and view for pricing screen

--- src/components/pricing/pricingCard.jsx
+++ src/components/pricing/pricingCard.jsx
@@ -4,14 +4,14 @@ import { ReactComponent as Ellipse } from "../../assets/svg/Ellipse324.svg";
 const pricingCard = ({duration, price}) => {
   return (
     <div className='pricingCardItem'>
-      <div>
+      <div className='pricingCardItemTop'>
         <div className='durationSection'>
             <h2>{duration} Plan</h2>
             <p>Charged {duration}</p>
         </div>
         <h2>N{price}</h2>
       </div>
-      <div>
+      <div className='pricingCardItemBottom'>
         <Ellipse/>
       </div>
     </div>
--- src/views/Pricing.jsx
+++ src/views/Pricing.jsx
@@ -5,58 +5,50 @@ import { ReactComponent as BlueTickMark } from "../assets/svg/BlueTickMark.svg";
 
 const Pricing = () => {
   return (
-    <div className='pricingContainer'>
-      <h2>
-      Select a plan that works for you
-      </h2>
-      <p>
-      Get access to unlimited resources, unlimited tests, create study plan that helps you prepare better for your JAMB
-      </p>
-      <div className='pricingFeatures'>
-        <div className='pricingFeaturesItem'>
-          <BlueTickMark/>
-          <p>Gamified tests</p>
-        </div>
-        <div className='pricingFeaturesItem'>
-        <BlueTickMark/>
-          <p>Unlimited resources</p>
-        </div>
-        <div className='pricingFeaturesItem'>
-          <BlueTickMark/>
-          <p>Progressive learning</p>
-        </div>
-        <div className='pricingFeaturesItem'>
-          <BlueTickMark/>
-          <p>24/7 support</p>
-        </div>
-        <div className='pricingFeaturesItem'>
-          <BlueTickMark/>
-          <p>Questions with explainable workings</p>
-        </div>
-        <div className='pricingFeaturesItem'>
-          <BlueTickMark/>
-          <p>Leader board</p>
-        </div>
-      </div>
-      <p>Select a plan that works for you</p>
-      <div className='pricingPlanSection'>
-          <div className="pricingCard">
-
+    <div>
+      <main className='pricingContainer'>
+        <h2>
+        Affordable Pricing To Help Ace Your Exams With Ease and Fun 
+        </h2>
+        <p>
+        Get access to unlimited resources, unlimited tests, create study plan that helps you prepare better for your JAMB
+        </p>
+        <div className='pricingFeatures'>
+          <div className='pricingFeaturesItem'>
+            <BlueTickMark/>
+            <p>Gamified tests</p>
           </div>
-          <div className="pricingCard">
-
+          <div className='pricingFeaturesItem'>
+          <BlueTickMark/>
+            <p>Unlimited resources</p>
           </div>
-          <div className="pricingCard">
-
+          <div className='pricingFeaturesItem'>
+            <BlueTickMark/>
+            <p>Progressive learning</p>
           </div>
-      </div>
-      <div>
-          <PricingCard duration="monthly" price="1000"/>
-          <PricingCard duration="quarterly" price="2500"/>
-          <PricingCard duration="yearly" price="9000"/>
-      </div>
+          <div className='pricingFeaturesItem'>
+            <BlueTickMark/>
+            <p>24/7 support</p>
+          </div>
+          <div className='pricingFeaturesItem'>
+            <BlueTickMark/>
+            <p>Questions with explainable workings</p>
+          </div>
+          <div className='pricingFeaturesItem'>
+            <BlueTickMark/>
+            <p>Leader board</p>
+          </div>
+        </div>
+        <p>Select a plan that works for you</p>
+        <div className='pricingCardContainer'>
+            <PricingCard duration="monthly" price="1000"/>
+            <PricingCard duration="quarterly" price="2500"/>
+            <PricingCard duration="yearly" price="9000"/>
+        </div>
+      </main>
       <Footer/>
     </div>
+    
   )
 }
 

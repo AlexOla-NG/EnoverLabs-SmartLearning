f569f49371edc77373a8cc8ddc7155bddf44435c
Alex Oweka <alexanderolalekanoweka@gmail.com>
2023-03-11 10:04:51 +0100

    🔥feat: added persistent navbar to outlet wrapper

--- src/components/shared/OutletWrapper.jsx
+++ src/components/shared/OutletWrapper.jsx
@@ -1,29 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 import { NavLink, Outlet } from "react-router-dom";
+import { styled, useTheme } from "@mui/material/styles";
+
 import {
-	AppBar,
 	Box,
 	CssBaseline,
+	Divider,
+	Drawer,
 	IconButton,
-	List,
-	ListItem,
-	ListItemButton,
-	ListItemIcon,
-	ListItemText,
 	Toolbar,
-	Typography,
 } from "@mui/material";
-import Drawer from "@mui/material/Drawer";
+import MuiAppBar from "@mui/material/AppBar";
+import ChevronLeftIcon from "@mui/icons-material/ChevronLeft";
+import ChevronRightIcon from "@mui/icons-material/ChevronRight";
 import MenuIcon from "@mui/icons-material/Menu";
 
 import TextIcon from "../card/TextIcon";
 
+import { ReactComponent as SearchIcon } from "../../assets/svg/search-icon.svg";
+import { ReactComponent as NotificationIcon } from "../../assets/svg/notifications-icon.svg";
+import { ReactComponent as ChevronDownIcon } from "../../assets/svg/chevron-down-icon.svg";
+import AvatarIcon from "../../assets/images/avatar.png";
 import { ReactComponent as HomeIcon } from "../../assets/svg/home-icon.svg";
 import { ReactComponent as ResourcesIcon } from "../../assets/svg/resources-icon.svg";
 import { ReactComponent as TestIcon } from "../../assets/svg/test-icon.svg";
 
 // TODO: stopped here
-// transfer PersistentDrawer contents here
+// add menu to chevronDown icon
+// style sidebar
+
+// STUB: define drawerwidth & items
+const drawerWidth = 240;
 
 const drawerItems = [
 	{ text: "dashboard", icon: <HomeIcon /> },
@@ -31,110 +38,164 @@ const drawerItems = [
 	{ text: "take a test", icon: <TestIcon /> },
 ];
 
-const OutletWrapper = ({
-	children,
-	drawerWidth,
-	mobileOpen,
-	handleDrawerToggle,
-}) => {
+// STUB: setup custom components for main, appbar & drawerheader
+const Main = styled("main", { shouldForwardProp: (prop) => prop !== "open" })(
+	({ theme, open }) => ({
+		flexGrow: 1,
+		padding: "24px 0 24px",
+		transition: theme.transitions.create("margin", {
+			easing: theme.transitions.easing.sharp,
+			duration: theme.transitions.duration.leavingScreen,
+		}),
+		marginLeft: `-${drawerWidth}px`,
+		[theme.breakpoints.up("sm")]: {
+			width: `calc(100% - ${drawerWidth}px)`,
+			marginLeft: "auto",
+		},
+		...(open && {
+			transition: theme.transitions.create("margin", {
+				easing: theme.transitions.easing.easeOut,
+				duration: theme.transitions.duration.enteringScreen,
+			}),
+			marginLeft: 0,
+		}),
+	})
+);
+
+const AppBar = styled(MuiAppBar, {
+	shouldForwardProp: (prop) => prop !== "open",
+})(({ theme, open }) => ({
+	transition: theme.transitions.create(["margin", "width"], {
+		easing: theme.transitions.easing.sharp,
+		duration: theme.transitions.duration.leavingScreen,
+	}),
+	...(open && {
+		width: `calc(100% - ${drawerWidth}px)`,
+		marginLeft: `${drawerWidth}px`,
+		transition: theme.transitions.create(["margin", "width"], {
+			easing: theme.transitions.easing.easeOut,
+			duration: theme.transitions.duration.enteringScreen,
+		}),
+	}),
+}));
+
+const DrawerHeader = styled("div")(({ theme }) => ({
+	display: "flex",
+	alignItems: "center",
+	padding: theme.spacing(0, 1),
+	// necessary for content to be below app bar
+	...theme.mixins.toolbar,
+	justifyContent: "flex-end",
+}));
+
+const OutletWrapper = ({ children }) => {
+	const theme = useTheme();
+	const [open, setOpen] = useState(false);
+
+	// STUB: controls for toggling drawer
+	const handleDrawerOpen = () => {
+		setOpen(true);
+	};
+
+	const handleDrawerClose = () => {
+		setOpen(false);
+	};
+
 	const drawer = (
 		<div>
-			<Toolbar disableGutters>
-				<div onClick={handleDrawerToggle} className="navbar-title">
-					<span>Smart</span>
-					<span>Learning</span>
-				</div>
-			</Toolbar>
-			<div>
-				{drawerItems.map((item, index) => {
-					return <TextIcon key={index} {...item} />;
-				})}
-			</div>
+			{drawerItems.map((item, index) => {
+				return <TextIcon key={index} {...item} />;
+			})}
 		</div>
 	);
+
 	return (
 		<Box sx={{ display: "flex" }}>
 			<CssBaseline />
 			<AppBar
 				position="fixed"
+				open={open}
 				sx={{
 					backgroundColor: "#fff",
 					boxShadow: "unset",
-					width: { sm: `calc(100% - ${drawerWidth}px)` },
-					ml: { sm: `${drawerWidth}px` },
 				}}
 			>
-				<Toolbar disableGutters>
+				<Toolbar>
 					<nav className="nav">
-						<div className="container">
-							<IconButton
-								color="inherit"
-								aria-label="open drawer"
-								edge="start"
-								onClick={handleDrawerToggle}
-								sx={{ mr: 2, display: { sm: "none" } }}
+						<div className="container outlet-nav">
+							<Box
+								sx={{
+									display: "flex",
+									alignItems: "center",
+									gap: "0.76rem",
+								}}
 							>
-								<MenuIcon />
-							</IconButton>
-							<NavLink to="/" className="navbar-title">
-								<span>Smart</span>
-								<span>Learning</span>
-							</NavLink>
+								<IconButton
+									color="#000"
+									aria-label="open drawer"
+									onClick={handleDrawerOpen}
+									edge="start"
+									sx={{
+										p: 0,
+										...(open && { display: "none" }),
+									}}
+								>
+									<MenuIcon />
+								</IconButton>
+								<NavLink
+									to="/"
+									className="navbar-title controls"
+								>
+									<span>Smart</span>
+									<span>Learning</span>
+								</NavLink>
+							</Box>
+
+							<div className="navbar-controls">
+								<SearchIcon />
+								<NotificationIcon />
+								<img
+									src={AvatarIcon}
+									alt="avatar"
+									style={{ width: "32px", height: "32px" }}
+								/>
+								<ChevronDownIcon />
+							</div>
 						</div>
 					</nav>
 				</Toolbar>
 			</AppBar>
-			<Box
-				component="nav"
-				sx={{ width: { sm: drawerWidth }, flexShrink: { sm: 0 } }}
-				aria-label="mailbox folders"
-			>
-				{/* The implementation can be swapped with js to avoid SEO duplication of links. */}
-				<Drawer
-					variant="temporary"
-					open={mobileOpen}
-					onClose={handleDrawerToggle}
-					ModalProps={{
-						keepMounted: true, // Better open performance on mobile.
-					}}
-					sx={{
-						display: { xs: "block", sm: "none" },
-						"& .MuiDrawer-paper": {
-							boxSizing: "border-box",
-							width: drawerWidth,
-						},
-					}}
-				>
-					{drawer}
-				</Drawer>
-				<Drawer
-					// variant="temporary"
-					variant="permanent"
-					sx={{
-						display: { xs: "none", sm: "block" },
-						"& .MuiDrawer-paper": {
-							boxSizing: "border-box",
-							width: drawerWidth,
-						},
-					}}
-					// open
-					open={mobileOpen}
-					onClose={handleDrawerToggle}
-				>
-					{drawer}
-				</Drawer>
-			</Box>
-			<Box
-				component="main"
+
+			<Drawer
 				sx={{
-					flexGrow: 1,
-					p: 3,
-					width: { sm: `calc(100% - ${drawerWidth}px)` },
+					width: drawerWidth,
+					flexShrink: 0,
+					"& .MuiDrawer-paper": {
+						width: drawerWidth,
+						boxSizing: "border-box",
+					},
 				}}
+				variant="persistent"
+				anchor="left"
+				open={open}
 			>
-				<Toolbar />
+				<DrawerHeader>
+					<IconButton onClick={handleDrawerClose}>
+						{theme.direction === "ltr" ? (
+							<ChevronLeftIcon />
+						) : (
+							<ChevronRightIcon />
+						)}
+					</IconButton>
+				</DrawerHeader>
+				<Divider />
+				{drawer}
+			</Drawer>
+
+			<Main open={open}>
+				<DrawerHeader />
 				<Outlet>{children}</Outlet>
-			</Box>
+			</Main>
 		</Box>
 	);
 };

ba413d588270e2bb840e62c0bc6f12725fb61320
Alex Oweka <alexanderolalekanoweka@gmail.com>
2023-03-18 09:58:19 +0100

    🧹chore: added test component to outlewrapper

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
-import { useState } from "react";
-import { Outlet, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Navbar from "./components/navigation/Navbar";
 import Home from "./views/Home";
 import Pricing from "./views/Pricing";
@@ -11,38 +10,18 @@ import SignUp from "./views/SignUp";
 import Error from "./views/Error";
 import OutletWrapper from "./components/shared/OutletWrapper";
 
-const drawerWidth = 240;
-
 const App = () => {
-	// const [mobileOpen, setMobileOpen] = useState(false);
-
-	// STUB: toggle drawer
-	// const handleDrawerToggle = () => {
-	// 	setMobileOpen(!mobileOpen);
-	// };
-
 	return (
 		<>
-			<Navbar
-			// drawerWidth={drawerWidth}
-			// handleDrawerToggle={handleDrawerToggle}
-			/>
+			<Navbar />
 			<Routes>
 				<Route path="/" element={<Home />} />
 				<Route path="pricing" element={<Pricing />} />
-				<Route
-					element={
-						<OutletWrapper
-						// drawerWidth={drawerWidth}
-						// mobileOpen={mobileOpen}
-						// handleDrawerToggle={handleDrawerToggle}
-						/>
-					}
-				>
+				<Route element={<OutletWrapper />}>
 					<Route path="study-plan" element={<StudyPlan />} />
 					<Route path="take-a-test" element={<TakeATest />} />
+					<Route path="take-a-test/:id" element={<Test />} />
 				</Route>
-				<Route path="take-a-test/:id" element={<Test />} />
 				<Route path="signup" element={<SignUp />} />
 				<Route path="signin" element={<SignIn />} />
 				<Route path="*" element={<Error />} />
